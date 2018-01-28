b80e5f89234af3e8fb4e1683403c8c60bc9f3cdf
Cody Moorhouse <cody.moorhouse@icloud.com>
2018-01-28 15:52:52 -0700

    Add service provider

---
+++ src/SecretaryServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace CodyMoorhouse\Secretary;
+
+use Illuminate\Support\ServiceProvider;
+
+/* Command */
+use CodyMoorhouse\Secretary\Commands\SectionCommand;
+
+class SecretaryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/config/secretary.php', 'secretary');
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        $this->loadRoutesFrom(__DIR__ .'/routes.php');
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/secretary.php' => config_path('secretary.php'),
+        ]);
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                SectionCommand::class
+            ]);
+        }
+    }
+}
+
+?>
