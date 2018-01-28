b6431835d14ac1ca68a341efde42822856854ae3
Cody Moorhouse <cody.moorhouse@icloud.com>
2018-01-28 15:51:17 -0700

    Add config file

---
+++ src/config/secretary.php
@@ -0,0 +1,60 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Previous Tables
+    |--------------------------------------------------------------------------
+    |
+    | Secretary is a note taking library that logs notes based on the logged in
+    | user. As such, you can customize the tables and the used column fields here.
+    |
+    */
+    /* Required */
+    'users_table'           =>  'users',
+    'users_table_id'        =>  'id',
+    'users_table_name'      =>  'name',
+
+    /* Optional */
+    'companies_table'       =>  'companies',
+    'companies_table_id'    =>  'id',
+    'companies_table_name'  =>  'name',
+
+    'roles_table'           =>  'roles',
+    'roles_table_id'        =>  'id',
+
+    /*
+    |--------------------------------------------------------------------------
+    | New Table Names
+    |--------------------------------------------------------------------------
+    |
+    | These are the new tables that will be created through the secretary
+    | migrations.
+    |
+    */
+    'sections_table'        =>  'sections',
+    'role_section_table'    =>  'role_section',
+    'notes_table'           =>  'notes',
+    'comments_table'        =>  'comments',
+    'media_table'           =>  'media',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Database Behaviours
+    |--------------------------------------------------------------------------
+    |
+    | The amount of transaction attempts on the database
+    |
+    */
+    'db_attempts'           =>  env('DB_ATTEMPTS', 5),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Storage
+    |--------------------------------------------------------------------------
+    |
+    | The storage disk where files will be saved to.
+    |
+    */
+    'disk'                  =>  'local',
+];
