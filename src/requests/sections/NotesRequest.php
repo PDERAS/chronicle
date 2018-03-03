20064a5a60b83846b788583dd9bb00265985948c
Cody Moorhouse <cody.moorhouse@icloud.com>
2018-03-03 15:46:05 -0700

    Fix copy paste error

--- src/requests/sections/NotesRequest.php
+++ src/requests/sections/NotesRequest.php
@@ -4,7 +4,7 @@ namespace CodyMoorhouse\Secretary\Requests\Sections;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class IndexRequest extends FormRequest
+class NotesRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
