ce613b26a28ac61fbcd9c524b06555813789768e
Cody Moorhouse <cody.moorhouse@icloud.com>
2018-03-04 17:20:28 -0700

    Apply some basic styling to the display

--- src/assets/js/components/Display.vue
+++ src/assets/js/components/Display.vue
@@ -1,18 +1,22 @@
 <template>
     <div class="secretary-display">
         <div class="secretary-header">
-            <div class="secretary-header-">
+            <div class="secretary-header">
                 {{ section.title }}
             </div>
             <div class="secretary-header-btns">
-
+                <button>R</button>
+                <button>F</button>
+                <button>B</button>
+                <button>N</button>
+                <button>L</button>
             </div>
         </div>
         <div class="secretary-content">
             <div class="secretary-note-wrapper" v-for="note in notes">
                 <div class="secretary-note-header">
-                    <span>{{ note.user.name }}</span>
-                    <span>{{ note.created_at }}</span>
+                    <span class="secretary-note-user">{{ note.user.name }}</span>
+                    <span class="secretary-note-time">{{ note.created_at }}</span>
                 </div>
                 <div class="secretary-note-content">
                     <div>{{ note.description }}</div>
@@ -64,3 +68,36 @@
         }
     }
 </script>
+
+<style lang="scss">
+    .secretary-header {
+        display: inline-block
+        font-family: Helvetica;
+        font-size: large;
+        padding: 5px;
+    }
+
+    .secretary-header-btns {
+        display: inline-block;
+        text-align: right;
+    }
+
+    .secretary-display {
+        font-family: Helvetica, sans-serif;
+    }
+
+    .secretary-note-wrapper {
+        padding: 10px 5px;
+    }
+
+    .secretary-note-time {
+        color: lighten(black, 60);
+        font-size: small;
+    }
+
+    .secretary-note-user {
+        color: black;
+        font-size: medium;
+        font-weight: bold;
+    }
+</style>
