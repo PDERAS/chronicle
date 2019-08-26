e2fe240c530081e70a99e46677996a3f85e440e1
Reed Jones <reed.jones@pderas.com>
2019-08-26 13:13:29 -0600

    added protected api auth routes

--- src/routes.php
+++ src/routes.php
@@ -5,42 +5,38 @@ namespace CodyMoorhouse\Chronicle;
 use Illuminate\Support\Facades\Route;
 
 Route::group([ 'namespace' => 'CodyMoorhouse\Chronicle\Controllers' ], function() {
+    $routes = function () {
+        Route::resource('/comments', 'CommentsController',
+            [ 'only' => [ 'destroy', 'store', 'update' ]
+        ]);
 
-    // API Routes
-    Route::middleware(config('chronicle.middlewares.api'))->group(function () {
-        Route::resource('/comments', 'CommentsController', [ 'only' => [
-            'destroy', 'store', 'update'
-        ]]);
+        Route::get('/media/{media}/download', 'MediaController@download');
+        Route::resource('/media/{media}', 'MediaController', [
+            'only' => ['destroy', 'store', 'update']
+        ]);
 
 
         Route::get('/notes/{note}/files', 'NotesController@getMedia');
-        Route::resource('/notes', 'NotesController', [ 'only' => [
-            'destroy', 'store', 'update'
-        ]]);
-
-
-        Route::get('/api/media/{media}/download', 'MediaController@download');
-        Route::resource('/media', 'MediaController', [
-            'parameters' => [
-                'media' => 'media'
-            ],
-            'only' => [
-                'destroy', 'store', 'update'
-            ]
+        Route::resource('/notes', 'NotesController',
+            [ 'only' => [ 'destroy', 'store', 'update' ]
         ]);
-    });
 
-    // Web Routes
-    Route::middleware(config('chronicle.middlewares.auth'))->group(function () {
-        Route::get('/media/{media}/download', 'MediaController@download');
-    });
-
-    // General Routes
-    Route::middleware(config('chronicle.middlewares.general'))->group(function () {
         Route::group([ 'prefix' => 'sections' ], function() {
             Route::get('/{section_tag}/notes', 'SectionsController@getNotes');
             Route::get('/{section_tag}', 'SectionsController@get');
             Route::get('/', 'SectionsController@index');
         });
-    });
+    };
+
+
+    // API Routes
+    Route::group([
+        'middleware' => config('chronicle.middlewares.api'),
+        'prefix' => config('chronicle.api_prefix')
+    ], $routes);
+
+    // Web Routes
+    Route::group([
+        'middleware' => config('chronicle.middlewares.auth')
+    ], $routes);
 });
